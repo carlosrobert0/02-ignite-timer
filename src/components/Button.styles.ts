d81a24ab381f8e27aede8d4130365ccd51e31d86
carlosrobert0 <carlosroberto15755@gmail.com>
2022-10-17 20:31:33 -0300

    colors and fonts

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -6,16 +6,10 @@ interface ButtonProps {
   variant: ButtonVariant;
 }
 
-const buttonVariants = {
-  primary: 'purple',
-  secondary: 'orange',
-  danger: 'red',
-  success: 'green'
-}
-
 export const ButtonContainer = styled.button<ButtonProps>`
   width: 100px;
   height: 40px;
 
-  background-color: ${({ theme }) => theme.primary};
+  background-color: ${({ theme }) => theme["green-500"]};
+  color: ${({ theme }) => theme.white}
 `
